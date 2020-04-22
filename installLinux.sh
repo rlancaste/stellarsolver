a2a13d8856c69123c750cafc3d27d3647992dcb3
Robert Lancaster <rlancaste@gmail.com>
2020-04-21 23:33:38 -0400

    Adding Linux Installer

---
+++ installLinux.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt -y install git cmake qt5-default libcfitsio-dev libgsl-dev
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
+make -j $(expr $(nproc) + 2)
+sudo make install
+
+cp ../SexySolverIcon.png $HOME/Pictures/
+
+# This will create a shortcut on the desktop for launching SexySolver
+##################
+cat > $HOME/Desktop/SexySolver.desktop <<- EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Icon[en_US]=$HOME/Pictures/SexySolverIcon.png
+Icon=$HOME/Pictures/SexySolverIcon.png
+Exec=/usr/bin/SexySolver
+Name[en_US]=SexySolver
+Name=SexySolver
+EOF
+##################
