0cd1aa7693288c230f118b097b831fefc7931bc0
Robert Lancaster <rlancaste@gmail.com>
2020-05-30 01:38:57 -0400

    fixing linux install

--- sexysolver.pc.cmake
+++ sexysolver.pc.cmake
@@ -3,11 +3,11 @@ exec_prefix=@CMAKE_INSTALL_PREFIX@
 libdir=@PKG_CONFIG_LIBDIR@
 includedir=@INCLUDE_INSTALL_DIR@
 
-Name: libsexysolver
+Name: sexysolver
 Description: SexySolver Internal Sextraction and Plate Solving Library
 URL: https://github.com/rlancaste/sexysolver-tester
 Version: @SexySolver_VERSION@
 Libs: -L${libdir} @PKG_CONFIG_LIBS@
-Libs.private: -lz -lcfitsio
+Libs.private: -lsexysolver
 Cflags: -I${includedir} -I${includedir}/sexysolver
 
