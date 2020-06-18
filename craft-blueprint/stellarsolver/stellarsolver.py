68af9f7fb31b9f8b99956342ae2db6b953d3b998
Robert Lancaster <rlancaste@gmail.com>
2020-06-17 22:23:49 -0400

    Adding new library recipe

---
+++ craft-blueprint/stellarsolver/stellarsolver.py
@@ -0,0 +1,36 @@
+import glob
+from xml.etree import ElementTree as et
+
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.description = 'StellarSolver Sextractor and Astrometry.net based Library'
+        self.svnTargets['Latest'] = "https://github.com/rlancaste/stellarsolver.git"
+        self.targetInstSrc['Latest'] = ""
+
+        self.defaultTarget = 'Latest'
+    
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.runtimeDependencies["libs/qt5/qtbase"] = "default"
+        self.runtimeDependencies["libs/gsl"] = "default"
+        self.runtimeDependencies["libs/mman"] = "default"
+        self.runtimeDependencies["libs/cfitsio"] = "default"
+        self.runtimeDependencies["libs/zlib"] = "default"
+        self.runtimeDependencies["boost-regex"] = "default"
+        self.runtimeDependencies["libs/wcslib"] = "default"
+
+
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+        root = CraftCore.standardDirs.craftRoot()
+        craftLibDir = os.path.join(root,  'lib')
+        self.subinfo.options.configure.args = "-DCMAKE_INSTALL_PREFIX=" + root + " -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_MACOSX_RPATH=1 -DCMAKE_INSTALL_RPATH=" + craftLibDir
