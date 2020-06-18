a11c9c3595b5a8bd60dcccc1478bf299e0ab4d3d
Robert Lancaster <rlancaste@gmail.com>
2020-06-18 01:29:25 -0400

    updates to script and cmake module file

--- linux-scripts/removeStellarSolver.sh
+++ linux-scripts/removeStellarSolver.sh
@@ -5,6 +5,7 @@ sudo rm /usr/lib/stellarsolver.*
 sudo rm /usr/bin/StellarSolverTester
 sudo rm /usr/lib/pkgconfig/stellarsolver.pc
 sudo rm $PKG_CONFIG_PATH/stellarsolver.pc
+sudo rm $HOME/Desktop/StellarSolverTester.desktop
 
 # Just in case the user had sexysolver before the rename
 sudo rm -r /usr/include/sexysolver
@@ -12,3 +13,4 @@ sudo rm /usr/lib/sexysolver.*
 sudo rm /usr/bin/SexySolverTester
 sudo rm /usr/lib/pkgconfig/sexysolver.pc
 sudo rm $PKG_CONFIG_PATH/sexysolver.pc
+sudo rm $HOME/Desktop/SexySolverTester.desktop
