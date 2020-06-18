0f86e84f0a4108a451c81f6f84155ce4eec96b61
Robert Lancaster <rlancaste@gmail.com>
2020-06-17 22:06:14 -0400

    Rename Part 8

--- craft-blueprint/SexySolverTester/StellarSolverTester.py
+++ craft-blueprint/SexySolverTester/StellarSolverTester.py
@@ -6,8 +6,8 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.description = 'SexySolver Sextractor and Astrometry.net based Library Tester Program'
-        self.svnTargets['Latest'] = "https://github.com/rlancaste/sexysolver-tester.git"
+        self.description = 'StellarSolver Sextractor and Astrometry.net based Library Tester Program'
+        self.svnTargets['Latest'] = "https://github.com/rlancaste/stellarsolver.git"
         self.targetInstSrc['Latest'] = ""
 
         self.defaultTarget = 'Latest'
@@ -33,11 +33,11 @@ class Package(CMakePackageBase):
         CMakePackageBase.__init__(self)
         root = CraftCore.standardDirs.craftRoot()
         craftLibDir = os.path.join(root,  'lib')
-        self.subinfo.options.configure.args = "-DCMAKE_INSTALL_PREFIX=" + root + " -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_MACOSX_RPATH=1 -DCMAKE_INSTALL_RPATH=" + craftLibDir
+        self.subinfo.options.configure.args = "-DCMAKE_INSTALL_PREFIX=" + root + " -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_MACOSX_RPATH=1 -DBUILD_TESTER=ON -DCMAKE_INSTALL_RPATH=" + craftLibDir
 
     def createPackage(self):
-        self.defines["executable"] = "bin\\SexySolverTester.exe"
-        self.defines["icon"] = os.path.join(self.packageDir(), "SexySolverInstallIcon.ico")
+        self.defines["executable"] = "bin\\StellarSolverTester.exe"
+        self.defines["icon"] = os.path.join(self.packageDir(), "StellarSolverInstallIcon.ico")
         if isinstance(self, AppxPackager):
-              self.defines["display_name"] = "SexySolverTester"
+              self.defines["display_name"] = "StellarSolverTester"
         return TypePackager.createPackage(self)
