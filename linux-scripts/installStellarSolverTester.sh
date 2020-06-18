1d29a932796569ed4dcd7f997f0b40df3790cdee
Robert Lancaster <rlancaste@gmail.com>
2020-06-17 23:44:48 -0400

    Script updates

--- linux-scripts/installStellarSolverTester.sh
+++ linux-scripts/installStellarSolverTester.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+#This gets the directory of the script, but note that the script is in a subdirectory, so to get the Repo's directory, we need ../
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../
+
+#This installs the dependencies
 if [ -f /usr/lib/fedora-release ]; then
   sudo dnf -y install git cmake qt5 cfitsio-devel gsl-devel wcslib-devel
 else
   sudo apt -y install git cmake qt5-default libcfitsio-dev libgsl-dev wcslib-dev
 fi
 
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTER=ON ../
+#This makes and installs the library
+mkdir $DIR/build
+cd $DIR/build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo $DIR
 make -j $(expr $(nproc) + 2)
 sudo make install
 
-cp ../tester/StellarSolverIcon.png $HOME/Pictures/
+#This copies the icon into the pictures directory for the section below
+cp $DIR/tester/StellarSolverIcon.png $HOME/Pictures/
 
 # This will create a shortcut on the desktop for launching StellarSolver
 ##################
