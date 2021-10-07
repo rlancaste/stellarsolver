1a14e2a109707ce4b3646b7179fbbb4fcc7fbd18
Dušan Poizl <nou.spiro@gmail.com>
2021-10-07 08:12:16 +0200

    Use C locale for numbers
    Tester application doesn't work on locales that use comma as decimal
    separator

--- tester/main.cpp
+++ tester/main.cpp
@@ -6,6 +6,9 @@ int main(int argc, char *argv[])
 {
 
     QApplication app(argc, argv);
+#if defined(__linux__)
+    setlocale(LC_NUMERIC, "C");
+#endif
     MainWindow *win = new MainWindow();
     app.exec();
     
