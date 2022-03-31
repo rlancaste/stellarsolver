1fbf95654cc1a5c249c2787dbcd5ddb6814b0815
Robert Lancaster <rlancaste@gmail.com>
2022-03-30 22:53:02 -0400

    updating recipes

--- craft-blueprints/stellarbatchsolver/stellarbatchsolver.py
+++ craft-blueprints/stellarbatchsolver/stellarbatchsolver.py
@@ -6,7 +6,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.description = 'StellarSolver Sextractor and Astrometry.net based Library Tester Program'
+        self.description = 'StellarSolver Star Extractor and Astrometry.net based Library Batch Solver Program'
         self.svnTargets['master'] = "https://github.com/rlancaste/stellarsolver.git"
         for ver in ['2.2']:
             self.targets[ver] = 'https://github.com/rlancaste/stellarsolver/archive/refs/tags/%s.tar.gz' % ver
@@ -31,11 +31,11 @@ class Package(CMakePackageBase):
         CMakePackageBase.__init__(self)
         root = str(CraftCore.standardDirs.craftRoot())
         craftLibDir = os.path.join(root,  'lib')
-        self.subinfo.options.configure.args = "-DCMAKE_INSTALL_PREFIX=" + root + " -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_MACOSX_RPATH=1 -DBUILD_TESTER=ON -DCMAKE_INSTALL_RPATH=" + craftLibDir
+        self.subinfo.options.configure.args = "-DCMAKE_INSTALL_PREFIX=" + root + " -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_MACOSX_RPATH=1 -DBUILD_BATCH_SOLVER=ON -DCMAKE_INSTALL_RPATH=" + craftLibDir
 
     def createPackage(self):
-        self.defines["executable"] = "bin\\StellarSolverTester.exe"
-        self.defines["icon"] = os.path.join(self.packageDir(), "StellarSolverInstallIcon.ico")
+        self.defines["executable"] = "bin\\StellarBatchSolver.exe"
+        self.defines["icon"] = os.path.join(self.packageDir(), "StellarBatchSolverInstallIcon.ico")
         if isinstance(self, AppxPackager):
-              self.defines["display_name"] = "StellarSolverTester"
+              self.defines["display_name"] = "StellarBatchSolver"
         return TypePackager.createPackage(self)
--- craft-blueprints/stellarsolver/stellarsolver.py
+++ craft-blueprints/stellarsolver/stellarsolver.py
@@ -1,16 +1,16 @@
 import glob
-from xml.etree import ElementTree as et
-
 import info
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
         self.description = 'StellarSolver Sextractor and Astrometry.net based Library'
-        self.svnTargets['Latest'] = "https://github.com/rlancaste/stellarsolver.git"
-        self.targetInstSrc['Latest'] = ""
-
-        self.defaultTarget = 'Latest'
+        self.svnTargets['master'] = "https://github.com/rlancaste/stellarsolver.git"
+        for ver in ['2.2']:
+            self.targets[ver] = 'https://github.com/rlancaste/stellarsolver/archive/refs/tags/%s.tar.gz' % ver
+            self.archiveNames[ver] = "stellarsolver-%s.tar.gz" % ver
+            self.targetInstSrc[ver] = "stellarsolver-%s" % ver
+        self.defaultTarget = '2.2'
     
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
@@ -22,15 +22,12 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["boost-regex"] = "default"
         self.runtimeDependencies["libs/wcslib"] = "default"
 
-
-
-
 from Package.CMakePackageBase import *
 
 
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        root = str(CraftCore.standardDirs.craftRoot())
-        craftLibDir = os.path.join(root,  'lib')
-        self.subinfo.options.configure.args = "-DCMAKE_INSTALL_PREFIX=" + root + " -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_MACOSX_RPATH=1 -DCMAKE_INSTALL_RPATH=" + craftLibDir
+        #root = CraftCore.standardDirs.craftRoot()
+        #craftLibDir = os.path.join(root,  'lib')
+        #self.subinfo.options.configure.args += f" -DCMAKE_MACOSX_RPATH=1 -DCMAKE_INSTALL_RPATH={craftLibDir}"
