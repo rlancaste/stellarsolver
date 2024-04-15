3f693a28e62919cfa085463deff6d96c6e84d9f8
Robert Lancaster <rlancaste@gmail.com>
2024-04-15 18:15:31 -0400

    Updating Linux Scripts for Qt6

---
+++ linux-scripts/installStellarSolverLibraryQt6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#This gets the directory of the script, but note that the script is in a subdirectory, so to get the Repo's directory, we need ../
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../
+
+#This installs the dependencies
+if [ -f /usr/lib/fedora-release ]; then
+  sudo dnf -y install git cmake qt6 cfitsio-devel gsl-devel wcslib-devel
+else
+  sudo apt -y install git cmake qt6-base-dev libgl1-mesa-dev libcfitsio-dev libgsl-dev wcslib-dev
+fi
+
+#This makes and installs the library
+mkdir -p $DIR/build
+cd $DIR/build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo $DIR
+make -j $(expr $(nproc) + 2)
+sudo make install
